a664d3e30b402a389685025d2c6bf4dd2d1a5e8b
Mahesar-del <abdullahkhanmahesar9696@gmail.com>
2026-08-18 12:57:07 +0500

    abdullah

--- public/js/home.js
+++ public/js/home.js
@@ -119,6 +119,21 @@
         var slides = track.children.length;
         var dots = Array.prototype.slice.call(document.querySelectorAll('#tDots .dot'));
         var index = 0;
+        var autoplayTimer;
+
+        function startAutoplay() {
+            stopAutoplay();
+            autoplayTimer = setInterval(function() {
+                var isVideoPlaying = track.querySelector('.testi-media.is-playing-video');
+                if (!isVideoPlaying) {
+                    goTo(index + 1);
+                }
+            }, 4000); // Auto scroll every 4 seconds
+        }
+
+        function stopAutoplay() {
+            if (autoplayTimer) clearInterval(autoplayTimer);
+        }
 
         function goTo(i) {
             index = (i + slides) % slides;
@@ -134,6 +149,8 @@
                     v.currentTime = 0;
                 }
             });
+            
+            startAutoplay(); // Reset timer on manual interaction
         }
 
         dots.forEach(function (d, i) { d.addEventListener('click', function () { goTo(i); }); });
